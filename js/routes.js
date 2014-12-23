e9a16a80acf2355a5e78772a800abca1f2265226
Dave Milford <davemilford@gmail.com>
2014-12-22 22:30:21 -0700

    More content :)

--- js/routes.js
+++ js/routes.js
@@ -5,16 +5,16 @@
 define(['./app'], function(app) {
     'use strict';
     return app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/view1', {
-            templateUrl: 'partials/partial1.html',
-            controller: 'MyCtrl1'
+        $routeProvider.when('/home', {
+            templateUrl: 'partials/homepage.html',
+            controller: 'HomeCtrl'
         });
         $routeProvider.when('/view2', {
             templateUrl: 'partials/partial2.html',
             controller: 'MyCtrl2'
         });
         $routeProvider.otherwise({
-            redirectTo: '/view1'
+            redirectTo: '/home'
          });
     }]);
 });
