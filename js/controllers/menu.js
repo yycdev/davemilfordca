59d34187cd3a19acaaaca81e029da14902858710
Dave Milford <davemilford@gmail.com>
2014-12-24 22:10:15 -0700

    Refactor menu stuff into its own control

--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -1,4 +1,36 @@
 define(['./module'], function(controllers) {
     'use strict';
-    controllers.controller('MenuCtrl', [function($scope) {}]);
+
+    function menuController($scope, whoAmIMenuContents, whatIDoMenuContents)
+    {    	
+    	// Scrolls to the selected menu item on the page
+    	$(function() 
+    	{
+	        $('a[href*=#]:not([href=#])').click(function() 
+	        {
+	            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) 
+	            {
+	                var target = $(this.hash);
+	                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+	                if (target.length) 
+	                {
+	                    $('html,body').animate({
+	                        scrollTop: target.offset().top
+	                    }, 1000);
+	                    return false;
+	                } // if
+	            } // if
+        	}); // function
+  		}); // function
+
+    	$scope.whoamidropdownlist = whoAmIMenuContents;
+    	$scope.whatidodropdownlist = whatIDoMenuContents;
+    }
+
+    menuController.$inject=['$scope','whoAmIMenuContents', 'whatIDoMenuContents'];
+
+    controllers.controller('MenuCtrl', ['$scope', 'whoAmIMenuContents', 'whatIDoMenuContents', menuController]);
+    	  
+
+
 });
--- js/controllers/whoami.js
+++ js/controllers/whoami.js
@@ -1,32 +1,12 @@
 define(['./module'], function(controllers) {
     'use strict';
 
-    function whoAmIController($scope, whoAmIMenuContents, whatIDoMenuContents)
+    function whoAmIController($scope)
     {
-    	// Scrolls to the selected menu item on the page
-    	$(function() 
-    	{
-	        $('a[href*=#]:not([href=#])').click(function() 
-	        {
-	            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) 
-	            {
-	                var target = $(this.hash);
-	                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-	                if (target.length) 
-	                {
-	                    $('html,body').animate({
-	                        scrollTop: target.offset().top
-	                    }, 1000);
-	                    return false;
-	                } // if
-	            } // if
-        	}); // function
-  		}); // function
 
-  		$scope.whoamidropdownlist = whoAmIMenuContents;
     } // whoAmIController
 
-    whoAmIController.$inject=['$scope','whoAmIMenuContents', 'whatIDoMenuContents'];
+    whoAmIController.$inject=['$scope'];
 
-    controllers.controller('WhoAmICtrl', ['$scope', 'whoAmIMenuContents', 'whatIDoMenuContents', whoAmIController]);
+    controllers.controller('WhoAmICtrl', ['$scope', whoAmIController]);
 });
