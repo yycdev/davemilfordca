0f3b1a27d8d5d2c9e0774e1705232c246b4ad977
Dave Milford <davemilford@gmail.com>
2014-12-26 17:31:20 -0700

    Finish up music section

--- js/main.js
+++ js/main.js
@@ -9,12 +9,12 @@ require.config({
         'angular-route': '//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-route.min',
         'angular-animate': '//ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-animate.min',
         'angular-touch': 'https://code.angularjs.org/1.3.8/angular-touch.min',
-        'angular-strap': '//cdnjs.cloudflare.com/ajax/libs/angular-strap/2.1.5/angular-strap.min',
-        'angular-strap-tpl': '//cdnjs.cloudflare.com/ajax/libs/angular-strap/2.1.5/angular-strap.tpl.min',
+        'angular-strap': 'lib/angular-strap/angular-strap.min',
+        'angular-strap-tpl': 'lib/angular-strap/angular-strap.tpl.min',
         'angular-scroll': 'lib/angular-scroll/angular-scroll.min',
         'domReady': 'lib/requirejs-domready/domReady',
         'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
-        'gsap': '//cdnjs.cloudflare.com/ajax/libs/gsap/1.10.3/TweenMax.min'
+        'gsap': 'lib/TweenMax.min'
     },
     /**
     * for libs that either do not support AMD out of the box, or
