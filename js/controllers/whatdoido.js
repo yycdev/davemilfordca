604147e58174de744e81069affd95c5e4f9f2124
Dave Milford <davemilford@gmail.com>
2014-12-27 00:09:41 -0700

    Add toolbox, and popovers

--- js/controllers/whatdoido.js
+++ js/controllers/whatdoido.js
@@ -1,4 +1,7 @@
 define(['./module'], function(controllers) {
     'use strict';
-    controllers.controller('WhatDoIDoCtrl', [function($scope) {}]);
+    controllers.controller('WhatDoIDoCtrl', ['$scope', function($scope) {
+    	// angular-strap needs this for some reason
+    	$scope.popover = {"title": "",  "content": " ", "testing": "blah"};
+    }]);
 });
